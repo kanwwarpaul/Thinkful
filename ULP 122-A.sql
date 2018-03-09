39cced4d91a173f216b56f13abacb7979f2dcd9f
Kanwar Paul <kanwwarpaul@hotmail.com>
2018-03-08 17:01:28 -0800

    DRILL SQL - BIKE SHARE - UNIT 2LESSON 2

---
+++ ULP 122-A.sql
@@ -0,0 +1,9 @@
+select 
+	trip_id,
+	duration
+from 
+	trips
+where 
+	duration > 500
+order by
+	duration
---
+++ ULP 122-C.sql
@@ -0,0 +1,6 @@
+select
+	MinTemperatureF
+from 
+	weather
+where 
+	ZIP = 94301 and Events= 'Rain';
